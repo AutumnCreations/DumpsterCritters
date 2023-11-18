20a9c33d36f821c921a90ea1a57ae062744cba4f
itsthebishop206 <itsthebishop206@gmail.com>
2023-11-18 10:27:45 -0800

    UI sound + Event Routing

--- Assets/Scripts/FoodContainer.cs
+++ Assets/Scripts/FoodContainer.cs
@@ -28,7 +28,7 @@ public class FoodContainer : InteractableContainer
     Image fillTimer;
 
     [BoxGroup("Audio")]
-    public string FMODEvent;
+    public string PickupEvent;
 
     float timePassed = 0;
 
@@ -53,7 +53,7 @@ public class FoodContainer : InteractableContainer
         if (currentObject != null)
         {
             currentObject.PickUp(player.grabPoint, true);
-            FMODUnity.RuntimeManager.PlayOneShot(FMODEvent, transform.position);
+            FMODUnity.RuntimeManager.PlayOneShot(PickupEvent, transform.position);
             currentObject = null;
             StartCoroutine(RespawnFruit());
         }
